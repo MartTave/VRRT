34c8dcce7c0a0e3b09d57c52ac46ae35b59203c9
MartTave <55973969+MartTave@users.noreply.github.com>
2025-06-04 11:42:02 +0200

    New calibration working !

--- source/stereo/take_picture.py
+++ source/stereo/take_picture.py
@@ -1,13 +1,15 @@
 import os
+
 import cv2 as cv
 
-LEFT_ID = 4
-RIGHT_ID = 2
+LEFT_ID = 2
+RIGHT_ID = 4
 IMAGE_FOLDER = "./data/pics"
 
 LEFT_FOLDER = os.path.join(IMAGE_FOLDER, "left")
 RIGHT_FOLDER = os.path.join(IMAGE_FOLDER, "right")
 STEREO_FOLDER = os.path.join(IMAGE_FOLDER, "stereo")
+
 
 def create_folders():
     if not os.path.exists(LEFT_FOLDER):
@@ -20,17 +22,18 @@ def create_folders():
         os.mkdir(STEREO_FOLDER)
         pass
 
-def set_cap_property(cap):
 
+def set_cap_property(cap):
     # Disable autofocus (0 = manual, 1 = auto)
     cap.set(cv.CAP_PROP_AUTOFOCUS, 0)
 
-    cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
+    cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*"MJPG"))
 
     cap.set(cv.CAP_PROP_FOCUS, 0)  # 50 = mid-range focus
     cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
     pass
+
 
 create_folders()
 
@@ -42,8 +45,9 @@ set_cap_property(cap_right)
 
 
 def build_filename(filename, index):
-    splitted = filename.split('.')
+    splitted = filename.split(".")
     return ".".join(splitted[:-1]) + f"_{index}." + splitted[-1]
+
 
 def update_index(filepath, curr_index=0):
     full_filepath = build_filename(filepath, curr_index)
@@ -53,15 +57,18 @@ def update_index(filepath, curr_index=0):
     else:
         return update_index(filepath, curr_index + 1)
 
+
 leftIndex = update_index(os.path.join(LEFT_FOLDER, "picture.png"))
 rightIndex = update_index(os.path.join(RIGHT_FOLDER, "picture.png"))
 stereoIndex = update_index(os.path.join(STEREO_FOLDER, "left.png"))
 
-while True:
+index = 0
 
+
+while True:
     ret_left, frame_left = cap_left.read()
     ret_right, frame_right = cap_right.read()
-
+    tookPic = False
     if not ret_left or not ret_right:
         raise Exception(f"Something went wrong during frame capture. Left : {ret_left} right: {ret_right}")
 
@@ -75,12 +82,11 @@ while True:
 
     key = cv.waitKey(1)
 
-    if key == ord('q'):
+    if key == ord("q"):
         break
-    elif key == ord('a'):
+    elif key == ord("a"):
         cv.imwrite(build_filename(os.path.join(LEFT_FOLDER, "picture.png"), leftIndex), frame_left)
         leftIndex += 1
-
 
         cv.imwrite(build_filename(os.path.join(RIGHT_FOLDER, "picture.png"), rightIndex), frame_right)
         rightIndex += 1
@@ -88,13 +94,20 @@ while True:
         cv.imwrite(build_filename(os.path.join(STEREO_FOLDER, "left.png"), stereoIndex), frame_left)
         cv.imwrite(build_filename(os.path.join(STEREO_FOLDER, "right.png"), stereoIndex), frame_right)
         stereoIndex += 1
-    elif key == ord('l'):
+        tookPic = True
+    elif key == ord("l"):
         cv.imwrite(build_filename(os.path.join(LEFT_FOLDER, "picture.png"), leftIndex), frame_left)
         leftIndex += 1
-    elif key == ord('r'):
+        tookPic = True
+    elif key == ord("r"):
         cv.imwrite(build_filename(os.path.join(RIGHT_FOLDER, "picture.png"), rightIndex), frame_right)
         rightIndex += 1
-    elif key == ord('s'):
+        tookPic = True
+    elif key == ord("s"):
         cv.imwrite(build_filename(os.path.join(STEREO_FOLDER, "left.png"), stereoIndex), frame_left)
         cv.imwrite(build_filename(os.path.join(STEREO_FOLDER, "right.png"), stereoIndex), frame_right)
         stereoIndex += 1
+        tookPic = True
+    if tookPic:
+        print(f"Took pic : {index}")
+        index += 1
