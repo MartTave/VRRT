3c9590bf7c5246f1be16e3529e140389f9eb0b21
MartTave <55973969+MartTave@users.noreply.github.com>
2025-06-05 15:29:30 +0200

    Not working perfectly, but advancing

--- source/stereo/depth_map.py
+++ source/stereo/depth_map.py
@@ -20,19 +20,20 @@ def get_depth_map(filename, img_left, img_right):
 
     map_right_x, map_right_y = (file["map_right_x"], file["map_right_y"])
 
-    stereo = cv2.StereoBM.create(numDisparities=64, blockSize=15)
+    stereo = cv2.StereoBM.create(numDisparities=512, blockSize=51)
 
-    stereo = cv2.StereoSGBM.create(
-        minDisparity=0,
-        numDisparities=64,
-        blockSize=3,
-        P1=8 * 3 * 3,
-        P2=32 * 3 * 3,
-        disp12MaxDiff=1,
-        uniquenessRatio=10,
-        speckleWindowSize=100,
-        speckleRange=32,
-    )
+    # stereo = cv2.StereoSGBM.create(
+    #     minDisparity=0,
+    #     numDisparities=64,
+    #     blockSize=7,
+    #     P1=0,
+    #     P2=0,
+    #     disp12MaxDiff=-1,
+    #     uniquenessRatio=10,
+    #     speckleWindowSize=0,
+    #     speckleRange=1,
+    #     mode=cv2.StereoSGBM_MODE_HH,
+    # )
 
     rect_img_left = cv2.remap(img_left, map_left_x, map_left_y, cv2.INTER_LINEAR)
 
@@ -96,44 +97,6 @@ def set_cap_property(cap):
     cap.set(cv2.CAP_PROP_FOCUS, 0)  # 50 = mid-range focus
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
-    rect_img_left = cv2.remap(img_left, map_left_x, map_left_y, cv2.INTER_LINEAR)
-
-    rect_img_right = cv2.remap(img_right, map_right_x, map_right_y, cv2.INTER_LINEAR)
-
-    resized_left = cv2.resize(rect_img_left, (640, 480))
-    resized_right = cv2.resize(rect_img_right, (640, 480))
-
-    concat = cv2.hconcat([resized_left, resized_right])
-    for i in range(0, 480, 10):
-        color = (255, 0, 0)
-        if i % 20 == 0:
-            color = (0, 0, 255)
-        cv2.line(concat, (0, i), (1280, i), color, 1)
-
-    cv2.imshow("Concat", concat)
-    while True:
-        key = cv2.waitKey(-1)
-        if key == ord("q"):
-            break
-
-    disparity = stereo.compute(rect_img_left, rect_img_right)
-
-    # Q is the 4x4 disparity-to-depth mapping matrix from stereoRectify
-    points_3D = cv2.reprojectImageTo3D(disparity, Q)
-
-    # Extract depth map (Z coordinate)
-    curr_depth = points_3D[:, :, 2]
-
-    disparity_vis = cv2.normalize(disparity, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # type: ignore
-
-    # Normalize for display
-    depth_vis = cv2.normalize(curr_depth, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # type: ignore
-
-    resized = cv2.resize(disparity_vis, (1280, 720))
-
-    cv2.imshow("disparity", rect_img_left)
-    cv2.waitKey(1)
     pass
 
 
@@ -150,6 +113,14 @@ def depth_map(filename):
 
     cap_right = cv2.VideoCapture(4)
 
+    cv2.namedWindow("Disparity Map")
+
+    nothing = lambda x: None
+
+
+    cv2.createTrackbar("NumDisparities", "Disparity Map", 1, 50, nothing)
+    cv2.createTrackbar("BlockSize", "Disparity Map", 5, 50, nothing)
+
     set_cap_property(cap_left)
     set_cap_property(cap_right)
 
@@ -160,13 +131,15 @@ def depth_map(filename):
     assert ret_left
     assert ret_right
 
-    stereo = cv2.StereoBM.create(numDisparities=16, blockSize=128)
+    stereo_left = cv2.StereoBM.create(
+        numDisparities=32, blockSize=7
+    )
+    filter = cv2.ximgproc.DisparityWLSFilter(matcher_left=stereo_left)
+    right_matcher = cv2.ximgproc.createRightMatcher(stereo_left)
 
     while True:
         ret, img_left = cap_left.read()
-
         ret, img_right = cap_right.read()
-
         img_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
         img_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
 
@@ -174,38 +147,26 @@ def depth_map(filename):
 
         rect_img_right = cv2.remap(img_right, map_right_x, map_right_y, cv2.INTER_LINEAR)
 
-        resized_left = cv2.resize(rect_img_left, (640, 480))
-        resized_right = cv2.resize(rect_img_right, (640, 480))
-
-        concat = cv2.hconcat([resized_left, resized_right])
-        for i in range(0, 480, 10):
-            color = (255, 0, 0)
-            if i % 20 == 0:
-                color = (0, 0, 255)
-            cv2.line(concat, (0, i), (1280, i), color, 1)
-
-        cv2.imshow("Concat", concat)
-        while True:
-            key = cv2.waitKey(-1)
-            if key == ord("q"):
-                break
-
-        disparity = stereo.compute(rect_img_left, rect_img_right)
+        disparity_left = stereo_left.compute(rect_img_left, rect_img_right)
+        disparity_right = right_matcher.compute(rect_img_right, rect_img_left)
 
         # Q is the 4x4 disparity-to-depth mapping matrix from stereoRectify
-        points_3D = cv2.reprojectImageTo3D(disparity, Q)
+        points_3D = cv2.reprojectImageTo3D(disparity_left, Q)
 
         # Extract depth map (Z coordinate)
         curr_depth = points_3D[:, :, 2]
 
-        disparity_vis = cv2.normalize(disparity, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # type: ignore
+
+        disparity_left = filter.filter(disparity_left, img_left, None, disparity_right)
+
+        disparity_vis = cv2.normalize(disparity_left, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # type: ignore
 
         # Normalize for display
         depth_vis = cv2.normalize(curr_depth, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)  # type: ignore
 
         resized = cv2.resize(disparity_vis, (1280, 720))
 
-        cv2.imshow("disparity", rect_img_left)
+        cv2.imshow("Disparity Map", resized)
         cv2.waitKey(1)
 
         pass
@@ -214,4 +175,4 @@ def depth_map(filename):
 img1 = cv2.imread("./data/pics/stereo/left_0.png")
 img2 = cv2.imread("./data/pics/stereo/right_0.png")
 
-get_depth_map("calib_2.npz", img1, img2)
+depth_map("calib_2.npz")
